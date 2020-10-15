9e4980f4486802d8d83ccc7aeb2864a1dd34d3c1
Grahame White <4whitg16@solent.ac.uk>
2020-10-15 13:43:24 +0100

    fixed some super linter warnings

--- myPracticeCourseWork/week 2/maven-test-exercise-hello_world/src/main/java/org/solent/com504/ood/App.java
+++ myPracticeCourseWork/week 2/maven-test-exercise-hello_world/src/main/java/org/solent/com504/ood/App.java
@@ -3,11 +3,10 @@ package org.solent.com504.ood;
 /**
  * Hello world!
  */
-public class App 
+public class App
 {
-    private int abc;
-    
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main(final String[] args)
+    {
+        System.out.println("Hello World!");
     }
 }
