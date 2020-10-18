5e6097af2fceb42cc9ea228190707dce948a892a
Grahame White <4whitg16@solent.ac.uk>
2020-10-18 18:47:31 +0100

    addressed more violations in example2

--- myPracticeCourseWork/week 2/maven-test-exercise-log4j/src/main/java/org/solent/com504/ood/MyTestClassLog4j.java
+++ myPracticeCourseWork/week 2/maven-test-exercise-log4j/src/main/java/org/solent/com504/ood/MyTestClassLog4j.java
@@ -11,7 +11,7 @@ public final class MyTestClassLog4j
     /**
      * field to hold a reference to the logger shared by all instances of this class.
      */
-    private static final Logger logger = LogManager.getLogger(MyTestClassLog4j.class);
+    private static final Logger LOGGER = LogManager.getLogger(MyTestClassLog4j.class);
 
     /**
      * Main function - entry point for the application.
@@ -29,13 +29,13 @@ public final class MyTestClassLog4j
     private void writeAboutMe()
     {
         System.out.println("This is a system out message from : " + MyTestClassLog4j.class);
-        logger.error("This is a log4j message from : " + MyTestClassLog4j.class);
+        LOGGER.error("This is a log4j message from : " + MyTestClassLog4j.class);
     }
 
     /**
-     *
-     * @param name
-     * @return
+     * Generate the "talking about" string for use in log files.
+     * @param name the string to be "talked about"
+     * @return returns a correctly structured "talking about" string
      */
     public String talkAboutMe(final String name)
     {
