740c2eb2615a647bc7bdc7a51f84a0fe63e76e9e
Grahame White <4whitg16@solent.ac.uk>
2020-10-18 17:54:33 +0100

    added missing package-info.java files

---
+++ myPracticeCourseWork/week 2/maven-test-exercise-log4j/src/main/java/org/solent/com504/ood/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Simple logger example using the log4j library.
+ */
+package org.solent.com504.ood;
---
+++ myPracticeCourseWork/week 2/maven-test-exercise-log4j/src/test/java/org/solent/com504/ood/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Unit tests for the simple logger example using the log4j library.
+ */
+package org.solent.com504.ood;
