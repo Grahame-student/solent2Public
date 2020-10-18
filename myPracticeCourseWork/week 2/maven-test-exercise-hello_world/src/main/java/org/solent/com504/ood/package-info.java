8590827ce916fe895010161925cbea5dd234b758
Grahame White <4whitg16@solent.ac.uk>
2020-10-18 13:54:18 +0100

    added javadoc comment to address linting violation

--- myPracticeCourseWork/week 2/maven-test-exercise-hello_world/src/main/java/org/solent/com504/ood/package-info.java
+++ myPracticeCourseWork/week 2/maven-test-exercise-hello_world/src/main/java/org/solent/com504/ood/package-info.java
@@ -1,6 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Simple hello world example
  */
 package org.solent.com504.ood;
