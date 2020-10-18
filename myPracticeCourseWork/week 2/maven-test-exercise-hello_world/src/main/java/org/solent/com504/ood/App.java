3e6569098655a0fc3b9110702023645489e4b058
Grahame White <4whitg16@solent.ac.uk>
2020-10-18 15:37:20 +0100

    added min code coverage target to maven project config

--- myPracticeCourseWork/week 2/maven-test-exercise-hello_world/src/main/java/org/solent/com504/ood/App.java
+++ myPracticeCourseWork/week 2/maven-test-exercise-hello_world/src/main/java/org/solent/com504/ood/App.java
@@ -9,9 +9,9 @@ public final class App
     {
         // unused constructor, only included to remove linter warning
     }
-    
+
     /**
-     * Main function - entry point for the application
+     * Main function - entry point for the application.
      * @param args List of arguments passed to the main function
      */
     public static void main(final String[] args)
